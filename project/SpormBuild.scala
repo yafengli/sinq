79a732a89e249f3a32982bd9622f87afa0cf428e
yafengli <ya_feng_li@163.com>
2012-12-11 10:33:26 +0800

    modify SpormBuild.scala.

--- project/SpormBuild.scala
+++ project/SpormBuild.scala
@@ -3,6 +3,19 @@ import sbt.Keys._
 
 object SpormBuild extends Build {
 
+  resolvers ++= Seq(
+    "Local Maven Repository" at "file:///e:/repository/",
+    "247" at "http://221.231.148.247/nexus/content/groups/public/",
+    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+  )
+
+  libraryDependencies ++= Seq(
+    "org.hibernate" % "hibernate-entitymanager" % "4.1.8.Final",
+    "org.hibernate" % "hibernate-c3p0" % "4.1.8.Final",
+    "postgresql" % "postgresql" % "9.1-901.jdbc4"
+  )
+
   lazy val sporm = Project(
     id = "sporm",
     base = file("."),
@@ -10,8 +23,19 @@ object SpormBuild extends Build {
       name := "sporm",
       organization := "org.example",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.9.2"
+      scalaVersion := "2.9.2",
       // add other settings here
+      resolvers ++= Seq(
+        "Local Maven Repository" at "file:///e:/repository/",
+        "247" at "http://221.231.148.247/nexus/content/groups/public/",
+        "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+        "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+      ),
+      libraryDependencies ++= Seq(
+        "org.hibernate" % "hibernate-entitymanager" % "4.1.8.Final",
+        "org.hibernate" % "hibernate-c3p0" % "4.1.8.Final",
+        "postgresql" % "postgresql" % "9.1-901.jdbc4"
+      )
     )
   )
 }
