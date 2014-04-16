7c909d013fd0ce7890927b155ccd0eb64335d0d7
ya_feng_li@163.com <ya_feng_li@163.com>
2014-04-16 11:45:36 +0800

    test link criteria query

--- src/main/scala/demo/ii/QueryExp.scala
+++ src/main/scala/demo/ii/QueryExp.scala
@@ -1,17 +1,27 @@
 package demo.ii
 
 import javax.persistence.EntityManager
+import javax.persistence.criteria.{Root, CriteriaQuery, CriteriaBuilder, Predicate}
 
-class QueryExp[T](val em: EntityManager) {
-
-
+class QueryExp[T: Manifest](val em: EntityManager) {
   def getType = implicitly[Manifest[T]].runtimeClass
 
   val cb = em.getCriteriaBuilder
   val cq = cb.createQuery(getType)
   val from = cq.from(getType)
 
-  def single(): Long = {
-
+  def where(call: (CriteriaBuilder, CriteriaQuery[_], Root[_]) => Predicate): QueryExp[T] = {
+    val list = List[Predicate](call(cb, cq, from))
+    cq.where(list: _*)
+    this
   }
+
+  def single(): Long = {
+    em.createQuery(cq).getSingleResult.asInstanceOf[Long]
+  }
+}
+
+trait QueryCall[T] {
+
+  def where(call: (CriteriaBuilder, CriteriaQuery[T]) => Predicate): QueryCall[T]
 }
---
+++ src/test/scala/test/DBIISpec.scala
@@ -0,0 +1,52 @@
+package test
+
+import org.koala.sporm.jpa.JPA
+import org.specs2._
+import javax.persistence.EntityManager
+import demo.ii.QueryExp
+import models.Book
+
+
+/**
+ * User: YaFengLi
+ * Date: 12-12-11
+ * Time: 上午11:08
+ */
+class DBIISpec extends Specification {
+  def init = {
+    JPA.initPersistenceName("default")
+    "call" must have size (4)
+  }
+
+  def is = s2"""
+
+ This is a specification to check the 'Hello world' string
+
+ The 'Hello world' string should
+   init db                                           ${H2DB.init}
+   init                                              $init
+   single                                            $single
+   stop db                                           ${H2DB.close}
+                                                     """
+
+  def single {
+    withEntityManager {
+      em =>
+        val qe = new QueryExp[Book](em)
+        qe.where((cb, cq, from) => cb.equal(from.get("price"), 12)).single()
+    }
+  }
+
+  def withEntityManager(call: (EntityManager) => Unit) {
+    val em = JPA.createEntityManager()
+    try {
+      call(em)
+    }
+    finally {
+      em.close()
+    }
+  }
+}
+
+
+
