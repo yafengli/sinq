1aeba10d5ecb9960d03c3a4680936e74c74a2a59
ya_feng_li@163.com <ya_feng_li@163.com>
2014-04-16 14:52:32 +0800

    LinkdDBSpec

--- src/test/scala/test/LinkDBSpec.scala
+++ src/test/scala/test/LinkDBSpec.scala
@@ -6,36 +6,35 @@ import javax.persistence.EntityManager
 import demo.ii.QueryExp
 import models.Book
 
-
 /**
  * User: YaFengLi
  * Date: 12-12-11
  * Time: 上午11:08
  */
-class DBIISpec extends Specification {
+class LinkDBSpec extends Specification {
   def init = {
-
+    JPA.initPersistenceName("default")
     "call" must have size (4)
   }
 
   def is = s2"""
 
- This is a specification to check the 'Hello world' string
+    This is a specification to check the 'Hello world' string
 
- The 'Hello world' string should
-   single                                            $single
-                                                     """
+    The 'Hello world' string should
+      open db                                           ${H2DB.init}
+      init                                              $init
+      single                                            $single
+      close db                                          ${H2DB.close}
+    """
 
   def single = {
-    H2DB.init
-    JPA.initPersistenceName("default")
     withEntityManager {
       em =>
         val qe = new QueryExp[Book](em)
         val sl = qe.where((cb, cq, from) => cb.equal(from.get("price"), 12)).single()
         println(sl)
     }
-    H2DB.close
     "Single"
   }
 
