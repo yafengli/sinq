c28cad3f853c2f7da0772d9b4a23980873e5fe6b
ya_feng_li@163.com <ya_feng_li@163.com>
2014-04-21 14:26:57 +0800

    criteriacomposer

--- src/main/scala/demo/ii/CriteriaComposer.scala
+++ src/main/scala/demo/ii/CriteriaComposer.scala
@@ -4,41 +4,36 @@ import scala.collection.mutable.ListBuffer
 import javax.persistence.criteria.{Selection, JoinType}
 import demo.ii.CriteriaOperator.CriteriaOperator
 import demo.ii.AggregateFunction._
-import demo.ii.{LogicOperator, NegationOperator}
 import demo.ii.NegationOperator.NegationOperator
 import demo.ii.LogicOperator.LogicOperator
 
 final class CriteriaComposer[T](val from: Class[T]) {
   var lastCallType = LastCallType.WHERE
-  val _select = ListBuffer[SelectContainer]()
-  val _where = ListBuffer[WhereContainer]()
-  val _join = ListBuffer[JoinContainer]()
+  val _select = ListBuffer[SelectContainer[_]]()
+  val _where = ListBuffer[WhereContainer[_]]()
+  val _join = ListBuffer[JoinContainer[_]]()
   val _having = ListBuffer[HavingContainer]()
   val _order = ListBuffer[OrderContainer]()
   val multiSelect = ListBuffer[Selection[_]]()
 
-  def select(): CriteriaComposer[T] = {
+  def where[V](attr: String, op: CriteriaOperator, v: V*): CriteriaComposer[T] = {
+    _where += WhereContainer(attr, op, v)
+    lastCallType = LastCallType.WHERE
     this
   }
 
-  def join(): CriteriaComposer[T] = {
-    this
-  }
-
-  def and(not: Boolean = false): CriteriaComposer[T] = {
+  def and(): CriteriaComposer[T] = {
     lastCallType match {
       case LastCallType.WHERE =>
         _where.size match {
           case size: Int if size > 0 =>
             _where(size - 1).logicOperator = LogicOperator.AND
-            if (not) _where(size - 1).notOperator = NegationOperator.NOT
           case _ =>
         }
       case LastCallType.HAVING =>
         _having.size match {
           case size: Int if size > 0 =>
             _having(size - 1).logicOperator = LogicOperator.AND
-            if (not) _having(size - 1).notOperator = NegationOperator.NOT
           case _ =>
         }
     }
@@ -46,13 +41,29 @@ final class CriteriaComposer[T](val from: Class[T]) {
     this
   }
 
-  def or(not: Boolean = false): CriteriaComposer[T] = {
-    _where.size match {
-      case size: Int if size > 0 =>
-        _where(size - 1).logicOperator = LogicOperator.OR
-        if (not) _where(size - 1).notOperator = NegationOperator.NOT
-      case _ =>
+  def or(): CriteriaComposer[T] = {
+    lastCallType match {
+      case LastCallType.WHERE =>
+        _where.size match {
+          case size: Int if size > 0 =>
+            _where(size - 1).logicOperator = LogicOperator.OR
+          case _ =>
+        }
+      case LastCallType.HAVING =>
+        _having.size match {
+          case size: Int if size > 0 =>
+            _having(size - 1).logicOperator = LogicOperator.OR
+          case _ =>
+        }
     }
+    this
+  }
+
+  def select(): CriteriaComposer[T] = {
+    this
+  }
+
+  def join(): CriteriaComposer[T] = {
     this
   }
 
@@ -69,16 +80,19 @@ final class CriteriaComposer[T](val from: Class[T]) {
   }
 }
 
-case class SelectContainer(val name: String, val alias: String = name, val aFun: AggregateFunction = null)
+case class SelectContainer[V](val name: String, val alias: String = name, val aFun: AggregateFunction = null)
 
 case class WhereContainer[V](val attr: String, val op: CriteriaOperator, val vs: V*) {
   var logicOperator: LogicOperator = _
   var notOperator: NegationOperator = _
 }
 
-case class JoinContainer(val attr: String, val joinType: JoinType)
+case class JoinContainer[V](val attr: String, val joinType: JoinType)
 
-case class HavingContainer(val attr: String, val op: CriteriaOperator)
+case class HavingContainer(val attr: String, val op: CriteriaOperator, val vs: V*) {
+  var logicOperator: LogicOperator = LogicOperator.AND
+  var notOperator: NegationOperator = null
+}
 
 case class GroupContainer(val attr: String, val op: CriteriaOperator)
 
