d98a2c42ecd1af772025254c0e7aee6492bed1d0
ya_feng_li@163.com <ya_feng_li@163.com>
2014-12-11 14:02:52 +0800

    Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -2,9 +2,7 @@ import java.io.File
 import scala.io.Source
 
 object Build{
-	val reg = "(.+)=(.+)".r
-	lazy val $ = scala.io.Source.fromFile(new File("project/build.properties")).getLines().map(it => {
-  	val m = reg.findFirstMatchIn(it).get
-  	(m.group(1).trim, m.group(2).trim)
-	}).toMap
+	 val reg = "(.+)=(.+)".r
+  val $ = Source.fromFile(new File("project/build.properties"))
+    .getLines().filter(reg.findFirstMatchIn(_).isDefined).map(reg.findFirstMatchIn(_).get).map(m => (m.group(1) -> m.group(2))).toMap
 }
