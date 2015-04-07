bf61ede0ad1e9e70c1acd9fcafa94b27a0fe5d6e
yafengli <ya_feng_li@163.com>
2015-04-07 17:42:23 +0800

    add like.

---
+++ sinq-jpa/src/main/scala/io/sinq/expression/Like.scala
@@ -0,0 +1,7 @@
+package io.sinq.expression
+
+import io.sinq.Column
+
+case class Like[T, +K](override val column: Column[K], override val paramValue: T) extends Tuple1Condition[T] {
+  override def link: String = "like"
+}
