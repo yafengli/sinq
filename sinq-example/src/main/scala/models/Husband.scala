eaa7c23210be679d4dd9277c5942ff76945ce619
ya_feng_li@163.com <ya_feng_li@163.com>
2016-01-01 14:12:40 +0800

    sinq is working.

--- sinq-example/src/main/scala/models/Husband.scala
+++ sinq-example/src/main/scala/models/Husband.scala
@@ -5,21 +5,16 @@ import javax.persistence._
 import scala.beans.BeanProperty
 
 /**
- * User: YaFengLi
- * Date: 12-12-3
- * Time: 下午1:28
- */
-@Entity
-@Table(name = "t_husband")
+  * User: YaFengLi
+  * Date: 12-12-3
+  * Time: 下午1:28
+  */
+@Entity @Table(name = "t_husband")
 case class Husband(@BeanProperty var name: String, @BeanProperty var age: Int) {
-  @Id
-  @GeneratedValue(strategy = GenerationType.TABLE, generator = "seq_t_husband")
-  @TableGenerator(name = "seq_t_husband", table = "seq_t_husband", allocationSize = 1)
-  @BeanProperty
+  @Id @GeneratedValue(strategy = GenerationType.TABLE, generator = "seq_t_husband") @TableGenerator(name = "seq_t_husband", table = "seq_t_husband", allocationSize = 1) @BeanProperty
   var id: Long = _
 
-  @OneToOne(fetch = FetchType.EAGER, optional = false)
-  @BeanProperty
+  @OneToOne(fetch = FetchType.EAGER, optional = false) @BeanProperty
   var teacher: Teacher = _
 
   def this() = this(null, -1)
--- sinq-example/src/main/scala/models/Student.scala
+++ sinq-example/src/main/scala/models/Student.scala
@@ -4,19 +4,13 @@ import javax.persistence._
 
 import scala.beans.BeanProperty
 
-
 @Entity
 @Table(name = "t_student")
 case class Student(@BeanProperty var name: String, @BeanProperty var age: Int, @BeanProperty var address: String) {
-  @Id
-  @GeneratedValue(strategy = GenerationType.TABLE, generator = "seq_t_student")
-  @TableGenerator(name = "seq_t_student", table = "seq_t_student", allocationSize = 1)
-  @BeanProperty
+  @Id @GeneratedValue(strategy = GenerationType.TABLE, generator = "seq_t_student") @TableGenerator(name = "seq_t_student", table = "seq_t_student", allocationSize = 1) @BeanProperty
   var id: Long = _
 
-  @ManyToOne(fetch = FetchType.EAGER, optional = false)
-  @JoinColumn(name = "teacher_id")
-  @BeanProperty
+  @ManyToOne(fetch = FetchType.EAGER, optional = false) @JoinColumn(name = "teacher_id") @BeanProperty
   var teacher: Teacher = _
 
   def this() = this(null, -1, null)
